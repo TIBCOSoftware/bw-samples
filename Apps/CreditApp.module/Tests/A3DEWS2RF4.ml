0f7b96a1c9bd1964dfab024d7c7db3f5af39ec23
Vineet <vineet140@gmail.com>
2018-08-30 15:06:41 -0700

    GetCreditScore App

---
+++ Apps/CreditApp.module/Tests/A3DEWS2RF4.ml
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="ASCII"?>
+<emulation:BWTFile xmi:version="2.0" xmlns:xmi="http://www.omg.org/XMI" xmlns:emulation="http:///emulation.ecore">
+  <CurrentLoadedTest href="platform:/resource/CreditApp.module/Tests/CreditApp.module1.bwt#/"/>
+  <EmulationDataFile>ACED0005740022544553542D4649434F53636F72652D3730302D312D457863656C6C656E742E627774</EmulationDataFile>
+  <EmulationDataFile>ACED0005740022544553542D4649434F53636F72652D3830302D312D457863656C6C656E742E627774</EmulationDataFile>
+  <EmulationDataFile>ACED00057400154372656469744170702E6D6F64756C65312E627774</EmulationDataFile>
+</emulation:BWTFile>
