ea3a81b036e495b4a535a902161eabc815a86eb1
Vineet <vineet140@gmail.com>
2018-08-30 17:29:12 -0700

    deleting junk file

--- Apps/CreditApp.module/Tests/A3DEWS2RF4.ml
+++
@@ -1,8 +0,0 @@
-<?xml version="1.0" encoding="ASCII"?>
-<emulation:BWTFile xmi:version="2.0" xmlns:xmi="http://www.omg.org/XMI" xmlns:emulation="http:///emulation.ecore">
-  <CurrentLoadedTest href="platform:/resource/CreditApp.module/Tests/TEST-Experian-Score-2-Good.bwt#/"/>
-  <EmulationDataFile>ACED0005740022544553542D4649434F53636F72652D3730302D312D457863656C6C656E742E627774</EmulationDataFile>
-  <EmulationDataFile>ACED0005740022544553542D4649434F53636F72652D3830302D312D457863656C6C656E742E627774</EmulationDataFile>
-  <EmulationDataFile>ACED00057400154372656469744170702E6D6F64756C65312E627774</EmulationDataFile>
-  <EmulationDataFile>ACED000574001E544553542D457870657269616E2D53636F72652D322D476F6F642E627774</EmulationDataFile>
-</emulation:BWTFile>
